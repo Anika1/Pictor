17259ba119c1be49501791fcccd0c2f8d1d7baa6
jeffers97 <Sweetie@Audra-jefferss-MacBook-Pro.local>
2014-03-30 14:19:15 -0400

    color size changes

--- Pictor/XYZViewController.h
+++ Pictor/XYZViewController.h
@@ -7,8 +7,9 @@
 //
 
 #import <UIKit/UIKit.h>
+#import <Parse/Parse.h>
+@interface XYZViewController : UIViewController<UIImagePickerControllerDelegate, UINavigationControllerDelegate>{}
 
-@interface XYZViewController : UIViewController<UIImagePickerControllerDelegate, UINavigationControllerDelegate>
 
 @property (weak, nonatomic) IBOutlet UIToolbar *cameraButton;
 
@@ -16,6 +17,16 @@
 
 - (IBAction)cameraButtonPressed:(id)sender;
 @property (strong, nonatomic) IBOutlet UIImageView *imageView;
+
+- (BOOL)logInViewController:(PFLogInViewController *)logInController shouldBeginLogInWithUsername:(NSString *)username password:(NSString *)password;
+- (void)logInViewController:(PFLogInViewController *)logInController didLogInUser:(PFUser *)user;
+
+/// Sent to the delegate when the log in attempt fails.
+- (void)logInViewController:(PFLogInViewController *)logInController didFailToLogInWithError:(NSError *)error;
+
+/// Sent to the delegate when the log in screen is dismissed.
+- (void)logInViewControllerDidCancelLogIn:(PFLogInViewController *)logInController;
+
 
 
 
